5678352161fee1676a0401775edb271d53b2d6ce
MarvinGomez1 <marvin20002018@hotmail.com>
2023-12-04 21:10:09 -0600

    Registro de vacunacion completado

---
+++ HappyPets/CapaNegocio/ModeloVacunacion.cs
@@ -0,0 +1,30 @@
+﻿using CapaDatos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaNegocio
+{
+    public class ModeloVacunacion
+    {
+        DatosVacunacion datosVacunacion = new DatosVacunacion();
+
+        public string RegistrarVacuna(DatosVacunacion vacunacion)
+        {
+            string respuesta = "";
+
+            try
+            {
+                datosVacunacion.RegistroVacunacion(vacunacion);
+            }
+            catch (Exception ex)
+            {
+                respuesta = "Ha ocurrido un error: " + ex.Message;
+            }
+            return respuesta;
+        }
+        
+    }
+}
