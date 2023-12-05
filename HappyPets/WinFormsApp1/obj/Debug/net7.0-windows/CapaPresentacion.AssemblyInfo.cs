5a8667d7414e8be5511803e88481d104dda2af29
MarvinGomez1 <marvin20002018@hotmail.com>
2023-12-04 21:43:18 -0600

    Correcion de campos para el formulario vacunacion

--- HappyPets/WinFormsApp1/obj/Debug/net7.0-windows/CapaPresentacion.AssemblyInfo.cs
+++ HappyPets/WinFormsApp1/obj/Debug/net7.0-windows/CapaPresentacion.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CapaPresentacion")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79e68633f08ac3e2b074998b8e00e8deb4af5f48")]
 [assembly: System.Reflection.AssemblyProductAttribute("CapaPresentacion")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CapaPresentacion")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
